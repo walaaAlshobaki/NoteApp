bad4a6eb3ddbac0dd7a3174b18be31eb40a8da70
walaaِAlshobaki <walaa1471996>
2022-01-06 18:01:30 +0200

    the note app

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/note/NotesEvent.kt
@@ -0,0 +1,13 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.note
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOder
+
+sealed class NotesEvent{
+    data class Order(val noteOder: NoteOder):NotesEvent()
+    data class DeleteNote(val note: Note):NotesEvent()
+    object RestoreNote:NotesEvent()
+    object ToggleOrderSection:NotesEvent()
+
+
+}
